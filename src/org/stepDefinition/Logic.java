46e6206663ed87b8844859de6f106c979fb53a46
GreensIrene <albeenairene28@gmail.com>
2022-03-25 12:57:03 +0530

    password fucntionality

--- src/org/stepDefinition/Logic.java
+++ src/org/stepDefinition/Logic.java
@@ -2,4 +2,11 @@ package org.stepDefinition;
 
 public class Logic {
 
+	public static void passWord() {
+		System.out.println("password");
+	}
+
+	public static void main(String[] args) {
+     passWord();
+	}
 }
